1f14251c30a7b6d1bdddbb220a8788e567d0e168
Frozen Tundra <96592245+FrozenTundraTrader@users.noreply.github.com>
2021-12-29 13:03:50 -0500

    stable update
    removed tracking of lastIndex and other indicies
    removed input change tracking
    added VAP difference calc to reduce # of refreshes needed
    added max bar calc option
    removed always update

--- vap_mult.cpp
+++ vap_mult.cpp
@@ -10,16 +10,11 @@ SCSFExport scsf_ChangeVolAtPriceMult(SCStudyInterfaceRef sc)
 {
 	
 	SCInputRef DebugOn = sc.Input[0];
-	SCInputRef CalculateWhileScrolling = sc.Input[1];
 	SCInputRef LookbackInterval = sc.Input[2];
 	SCInputRef MagicNumber = sc.Input[3];
+	SCInputRef UseBarAverageInstead = sc.Input[4];
+	SCInputRef MinimalVapPercentageChangeThreshold = sc.Input[5];
 	SCString log_message;
-
-	// keys for persistent dictionaries
-	int lastIndexKey = 0;
-	int calculateWhileScrollingKey = 1;
-	int lookbackIntervalKey = 2;
-	int magicNumberKey = 3;
 	
     // Configuration
     if (sc.SetDefaults)
@@ -27,93 +22,68 @@ SCSFExport scsf_ChangeVolAtPriceMult(SCStudyInterfaceRef sc)
 		sc.GraphRegion = 0;
 		DebugOn.Name = "Debug Enabled?";
 		DebugOn.SetYesNo(0);
-
-		CalculateWhileScrolling.Name = "Calculate while scrolling?";
-		CalculateWhileScrolling.SetYesNo(1);
-		sc.SetPersistentInt(calculateWhileScrollingKey, 1);
-
 		LookbackInterval.Name = "Lookback Interval # Bars";
-		LookbackInterval.SetInt(6);
-		sc.SetPersistentInt(lookbackIntervalKey, 6);
-
+		LookbackInterval.SetInt(sc.IndexOfLastVisibleBar - sc.IndexOfFirstVisibleBar);
 		MagicNumber.Name = "Magic Multiplier (ticks)";
 		MagicNumber.SetFloat(0.3);
-		sc.SetPersistentFloat(magicNumberKey, 0.3);
-
+		UseBarAverageInstead.SetYesNo(0);
+		UseBarAverageInstead.Name = "Use bar averages instead of max visible bar for diff calculation";
+		MinimalVapPercentageChangeThreshold.Name = "Percentage that VAP must change to trigger re-calc";
+		MinimalVapPercentageChangeThreshold.SetFloat(0.30);
         return;
     }
 
-	int lastIndex;
-	int &lastIndexProcessed = sc.GetPersistentInt(lastIndexKey);
-
-	// Don't process on startups nor recalculations. 
-	if (sc.UpdateStartIndex == 0) { 
-		lastIndexProcessed = -1;
-		return; 
-	}
-
-	// set lastIndex according to last or visible bar
-	if (CalculateWhileScrolling.GetInt() == 1) {
-		sc.UpdateAlways = 1;
-		lastIndex = sc.IndexOfLastVisibleBar;
-	} else {
-		sc.UpdateAlways = 0;
-		// UpdateStartIndex has to be used when autoloop is set to 0 (default value)
-		lastIndex = sc.UpdateStartIndex;
-	}
-
-	// Check for input changes. If input got changed we want to recalculate
-	int input_changed = 0;
-	if (CalculateWhileScrolling.GetInt() != sc.GetPersistentInt(calculateWhileScrollingKey) ||
-		LookbackInterval.GetInt() != sc.GetPersistentInt(lookbackIntervalKey) ||
-		MagicNumber.GetFloat() != sc.GetPersistentFloat(magicNumberKey)) {
-			sc.SetPersistentInt(calculateWhileScrollingKey, CalculateWhileScrolling.GetInt());
-			sc.SetPersistentInt(lookbackIntervalKey, LookbackInterval.GetInt());
-			sc.SetPersistentInt(magicNumberKey, MagicNumber.GetFloat());
-			input_changed = 1;
-			log_message.Format("Input changed");
-			if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
-	}
-
-	// Process only when lastIndex or input changed
-	if (lastIndex == lastIndexProcessed && input_changed == 0) { 
-		return;
-	}
 	
-	log_message.Format("lastIndex=%d, sc.GetPersistentInt=%d", lastIndex, sc.GetPersistentInt(0));
+	int currentIndex = sc.UpdateStartIndex;  // used to be sc.Index
+	int lastIndex = sc.IndexOfLastVisibleBar;	
+	
+	log_message.Format("currentIndex=%d, lastIndex=%d, sc.Index=%d, sc.GetPersistentInt=%d", currentIndex, lastIndex, sc.Index, sc.GetPersistentInt(0));
 	if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
-
-	// Set new persistent lastIndex to be last one preventing future updates
-	lastIndexProcessed = lastIndex;
 	
 	// calc bar ranges 
-	float bar_diff_sums = 0;
 	float tmp_bar_diff = 0;
+	float max_bar_diff = 0;
+	float bar_diff_sums = 0;
 	int lookback_interval = LookbackInterval.GetInt();
 	
 	log_message.Format("Heading into for loop, lastIndex=%d, lookback_interval=%d", lastIndex,  lookback_interval);
 	if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
 	
-	for (int i=lastIndex - lookback_interval; i<lastIndex; i++) {
+	for (int i=lastIndex - lookback_interval; i<=lastIndex; i++) {
 		tmp_bar_diff = sc.BaseData[SC_HIGH][i] - sc.BaseData[SC_LOW][i];
-		log_message.Format("lastIndex=%d, i=%d, bar_diff=%f", lastIndex, i, tmp_bar_diff);
+		log_message.Format("lastIndex=%d, i=%d, tmp_bar_diff=%f", lastIndex, i, tmp_bar_diff);
 		if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
+		max_bar_diff = max(max_bar_diff, tmp_bar_diff);
 		bar_diff_sums += tmp_bar_diff;
 	}
 		
-	float bar_diff = bar_diff_sums / lookback_interval;
 	float magic_number = MagicNumber.GetFloat();
-	int vap = sc.Round(bar_diff / magic_number);
+	int vap = sc.Round(max_bar_diff / magic_number);
+	if (UseBarAverageInstead.GetInt() == 1) {
+		vap = sc.Round((bar_diff_sums/lookback_interval) / magic_number);
+	}
+
 	
 	// futures with tick sizes more than one penny will require us to adjust
 	// the formula we use to calculate visually appealing VAP:
 	if (sc.TickSize > 0.01) {
-		vap = sc.Round(bar_diff * magic_number * sc.TickSize);
+		vap = sc.Round(max_bar_diff * magic_number * sc.TickSize);
 	}
 	
 	// safety check for setting VAP
 	if (vap == 0) {
 		vap = 1;
+	}
+	
+	// prevent excessive VAP updates when change is minimal
+	// Malykubo suggests 30% or more change to trigger a VAP change
+	int vap_diff = sc.VolumeAtPriceMultiplier - vap;
+	float vap_change_perc = (float)abs(vap_diff) / (float)sc.VolumeAtPriceMultiplier;
+	log_message.Format("vap_diff=%d, perc change=%f", vap_diff, vap_change_perc);
+	if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
+	if (vap_change_perc < MinimalVapPercentageChangeThreshold.GetFloat()) {
+		// don't update, vap change too small 
+		return;  
 	}
 		
 	// Change VAP only on change
@@ -126,6 +96,7 @@ SCSFExport scsf_ChangeVolAtPriceMult(SCStudyInterfaceRef sc)
 		if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
 	}
 	
-	log_message.Format("Idx to calc from: %d, Bar High: %f, Bar Low: %f, Bar Diff: %f, VAP: %d", lastIndex, sc.High[lastIndex], sc.Low[lastIndex], bar_diff, vap);
+	log_message.Format("Idx to calc from: %d, Bar High: %f, Bar Low: %f, Max Bar Diff: %f, VAP: %d", lastIndex, sc.High[lastIndex], sc.Low[lastIndex], max_bar_diff, vap);
 	if (DebugOn.GetInt() == 1) sc.AddMessageToLog(log_message, 1);
 }
+
