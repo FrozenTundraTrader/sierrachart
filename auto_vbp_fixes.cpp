1478b7c63142591727154010b3ff44eaa4c0ac74
Frozen Tundra <96592245+FrozenTundraTrader@users.noreply.github.com>
2022-01-09 16:02:46 -0500

    initial commit
    This study automates the Volume By Price -> Ticks Per Volume bar setting as you switch symbols so that it remains visible and consumable despite dynamic price changes from symbol to symbol

---
+++ auto_vbp_fixes.cpp
@@ -0,0 +1,56 @@
+#include "sierrachart.h"
+
+SCDLLName("Frozen Tundra - Auto Volume by Price")
+
+/*
+    Written by Malykubo and Frozen Tundra in FatCat's Discord Room
+*/
+
+SCSFExport scsf_AutoVbP(SCStudyInterfaceRef sc)
+{
+
+    // which study to reference
+    SCInputRef i_StudyRef = sc.Input[0];
+
+    // magic number multiplier that will determine the granularity of VP bars we'll see as we switch symbols
+    // the lower the number, the thicker and fewer bars there will be
+    // the higher the magic number, the thinner and more VP bars there will be
+    SCInputRef i_TickMultiplier = sc.Input[1];
+
+    // logging object
+    SCString log_message;
+
+    // Configuration
+    if (sc.SetDefaults)
+    {
+        sc.GraphRegion = 0;
+        i_StudyRef.Name = "Select Target Study";
+        i_StudyRef.SetStudyID(0);
+        i_TickMultiplier.Name = "Magic Number Multiplier";
+        i_TickMultiplier.SetFloat(1.3);
+
+        return;
+    }
+
+    float vHigh, vLow, vDiff;
+    float vMultiplier = i_TickMultiplier.GetFloat();
+
+    // fetch the graph's price scale's high and low value so we can automate the Ticks setting on VbP
+    sc.GetMainGraphVisibleHighAndLow(vHigh, vLow);
+
+    // calc the range of visible prices
+    vDiff = (vHigh - vLow);
+
+    // divide range by magic number to get the desired VbP Ticks Per Bar value
+    int targetTicksPerBar = sc.Round(vDiff / vMultiplier);
+
+    //log_message.Format("H=%f, L=%f, Diff=%f, target=%d", vHigh, vLow, vDiff, targetTicksPerBar);
+    //sc.AddMessageToLog(log_message, 1);
+
+    int studyId = i_StudyRef.GetStudyID();
+
+    // VbP Ticks Per Volume Bar is input 32, ID 31
+    int inputIdx = 31;
+
+    sc.SetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, targetTicksPerBar);
+}
