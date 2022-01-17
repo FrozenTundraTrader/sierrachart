3a41191ecc5dff9cab43cd1af17f7e8f9c05eba7
MalyKubo <malykubo@gmail.com>
2022-01-17 19:03:51 +0100

    Added timeInterval for study recalculation

--- JIGSAW_Export.cpp
+++ JIGSAW_Export.cpp
@@ -9,7 +9,9 @@ SCDLLName("JIGSAW Export")
 SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
 {
 
+    SCString log_message;
     SCInputRef vwapStudyRef = sc.Input[0];
+    SCInputRef timeInterval = sc.Input[1];
 
     // Configuration
     if (sc.SetDefaults)
@@ -17,8 +19,18 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
         vwapStudyRef.Name = "Select VWAP Study";
         vwapStudyRef.SetStudyID(0);
 
+        timeInterval.Name = "Interval for recalculation in sec";
+        timeInterval.SetInt(60);
+
         return;
     }
+
+    // Recalculate every timeInterval sec
+    SCDateTime& lastDateTime = sc.GetPersistentSCDateTime(0);
+    if (sc.LatestDateTimeForLastBar.GetTimeInSeconds() < lastDateTime.GetTimeInSeconds() + timeInterval.GetInt()) {
+        return;
+    }
+    sc.SetPersistentSCDateTime(0, sc.LatestDateTimeForLastBar);
 
     int vwapValueIndex = 0;
     int vwapTopStdDevIndex = 1;
@@ -28,7 +40,6 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
     //Get vwap value subgraph from the vwapStudyRef
     if (sc.GetStudyArrayUsingID(vwapStudyRef.GetStudyID(), vwapValueIndex, vwapValueStudyArray) > 0 && vwapValueStudyArray.GetArraySize() > 0) {
         float lastValue = vwapValueStudyArray[sc.UpdateStartIndex];
-        SCString log_message;
         log_message.Format("VWAP: %f", lastValue);
         sc.AddMessageToLog(log_message, 1);
     }
@@ -37,7 +48,6 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
     //Get top std dev subgraph from the vwapStudyRef
     if (sc.GetStudyArrayUsingID(vwapStudyRef.GetStudyID(), vwapTopStdDevIndex, vwapTopStdDevStudyArray) > 0 && vwapTopStdDevStudyArray.GetArraySize() > 0) {
         float lastValue = vwapTopStdDevStudyArray[sc.UpdateStartIndex];
-        SCString log_message;
         log_message.Format("TOP STD DEV: %f", lastValue);
         sc.AddMessageToLog(log_message, 1);
     }
@@ -46,7 +56,6 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
     //Get bottom std dev subgraph from the vwapStudyRef
     if (sc.GetStudyArrayUsingID(vwapStudyRef.GetStudyID(), vwapBottomStdDevIndex, vwapBottomStdDevStudyArray) > 0 && vwapBottomStdDevStudyArray.GetArraySize() > 0) {
         float lastValue = vwapBottomStdDevStudyArray[sc.UpdateStartIndex];
-        SCString log_message;
         log_message.Format("BOTTOM STD DEV: %f", lastValue);
         sc.AddMessageToLog(log_message, 1);
     }
