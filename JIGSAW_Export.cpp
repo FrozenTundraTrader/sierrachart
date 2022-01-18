9d553feebda485f11933bf37e950e9f212d95533
Frozen Tundra <96592245+FrozenTundraTrader@users.noreply.github.com>
2022-01-18 16:09:18 -0500

    write to file implemented

--- JIGSAW_Export.cpp
+++ JIGSAW_Export.cpp
@@ -1,3 +1,4 @@
+#include "helpers.h"
 #include "sierrachart.h"
 
 SCDLLName("JIGSAW Export")
@@ -6,6 +7,42 @@ SCDLLName("JIGSAW Export")
     Written by Malykubo and Frozen Tundra in FatCat's Discord Room
 */
 
+const char *colors[147] = { "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen" };
+
+/*
+ *
+ * values to export:
+ *
+ * ovnH
+ * ovnL
+ * pdVWAP
+ * pdStdD+
+ * pdStdD-
+ * dVWAP
+ * dV+
+ * dV-
+ *
+ */
+
+// Log To File
+/* target CSV format
+ *  174'16,uPL,DarkGray
+ *  154'21,mP,LightSalmon
+ */
+void logf(SCStudyInterfaceRef sc, SCString log_message) {
+    unsigned int bytesWritten = 0;
+    int fileHandle;
+    int msgLength = 0;
+
+    msgLength = log_message.GetLength() + 2;
+    log_message = log_message + "\r\n";
+
+    SCString filePath = "C:\\SierraChart\\sc_log.txt";
+    sc.OpenFile(filePath, n_ACSIL::FILE_MODE_OPEN_TO_APPEND, fileHandle);
+    sc.WriteFile(fileHandle, log_message, msgLength, &bytesWritten);
+    sc.CloseFile(fileHandle);
+}
+
 SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
 {
 
@@ -13,9 +50,12 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
     SCInputRef vwapStudyRef = sc.Input[0];
     SCInputRef timeInterval = sc.Input[1];
 
+
     // Configuration
     if (sc.SetDefaults)
     {
+        sc.GraphRegion = 0;
+
         vwapStudyRef.Name = "Select VWAP Study";
         vwapStudyRef.SetStudyID(0);
 
@@ -25,10 +65,12 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
         return;
     }
 
+    Helper help(sc);
+
     // Recalculate every timeInterval sec
     SCDateTime& lastDateTime = sc.GetPersistentSCDateTime(0);
     if (sc.LatestDateTimeForLastBar.GetTimeInSeconds() < lastDateTime.GetTimeInSeconds() + timeInterval.GetInt()) {
-        return;
+        //return;
     }
     sc.SetPersistentSCDateTime(0, sc.LatestDateTimeForLastBar);
 
@@ -39,24 +81,33 @@ SCSFExport scsf_JigsawExport(SCStudyInterfaceRef sc)
     SCFloatArray vwapValueStudyArray;
     //Get vwap value subgraph from the vwapStudyRef
     if (sc.GetStudyArrayUsingID(vwapStudyRef.GetStudyID(), vwapValueIndex, vwapValueStudyArray) > 0 && vwapValueStudyArray.GetArraySize() > 0) {
-        float lastValue = vwapValueStudyArray[sc.UpdateStartIndex];
-        log_message.Format("VWAP: %f", lastValue);
-        sc.AddMessageToLog(log_message, 1);
+        double lastValue = vwapValueStudyArray[sc.UpdateStartIndex];
+        SCString fmtValue = sc.FormatGraphValue(lastValue, sc.BaseGraphValueFormat);
+        log_message.Format("%s,dV,DarkGray", fmtValue.GetChars());
+        help.dump(log_message);
+        logf(sc, log_message);
     }
 
     SCFloatArray vwapTopStdDevStudyArray;
     //Get top std dev subgraph from the vwapStudyRef
     if (sc.GetStudyArrayUsingID(vwapStudyRef.GetStudyID(), vwapTopStdDevIndex, vwapTopStdDevStudyArray) > 0 && vwapTopStdDevStudyArray.GetArraySize() > 0) {
-        float lastValue = vwapTopStdDevStudyArray[sc.UpdateStartIndex];
-        log_message.Format("TOP STD DEV: %f", lastValue);
-        sc.AddMessageToLog(log_message, 1);
+        double lastValue = vwapTopStdDevStudyArray[sc.UpdateStartIndex];
+        SCString fmtValue = sc.FormatGraphValue(lastValue, sc.BaseGraphValueFormat);
+        //log_message.Format("TOP STD DEV: %f", lastValue);
+        log_message.Format("%s,dV+,LightSalmon", fmtValue.GetChars());
+        help.dump(log_message);
+        logf(sc, log_message);
     }
 
     SCFloatArray vwapBottomStdDevStudyArray;
     //Get bottom std dev subgraph from the vwapStudyRef
     if (sc.GetStudyArrayUsingID(vwapStudyRef.GetStudyID(), vwapBottomStdDevIndex, vwapBottomStdDevStudyArray) > 0 && vwapBottomStdDevStudyArray.GetArraySize() > 0) {
         float lastValue = vwapBottomStdDevStudyArray[sc.UpdateStartIndex];
-        log_message.Format("BOTTOM STD DEV: %f", lastValue);
-        sc.AddMessageToLog(log_message, 1);
+        SCString fmtValue = sc.FormatGraphValue(lastValue, sc.BaseGraphValueFormat);
+        //log_message.Format("BOTTOM STD DEV: %f", lastValue);
+        log_message.Format("%s,dV-,DeepPink", fmtValue.GetChars());
+        help.dump(log_message);
+        logf(sc, log_message);
     }
+
 }
