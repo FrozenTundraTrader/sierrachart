44b256b203c3050def3ff2868cb5dfe8de822bd5
cgza <127050739+cgza@users.noreply.github.com>
2023-03-05 20:47:29 +0100

    Modifications/additions by carloz
    - added targeting more than one VbP study
    - added a min-step for the VbP's ticks-per-bar setting to trigger chart redraw
    - make sure the step parameter is always a multiple of the VAP multiplier setting in Chart Settings to avoid VbPs not redrawing.
    - when the needed resolution goes below the step (i.e if step is 8 ticks but VbPs need less ticks per bar) ensure that the VbP's ticks-per-bar setting is also a multiple of the VAP multiplier; for example, if step is 8 and VAP is 2 and the needed resolution needs to go below 8, then the VbP's ticks-per-bar setting will be gradually decreased to 6, 4 and 2.

--- auto_vbp_fixes.cpp
+++ auto_vbp_fixes.cpp
@@ -8,65 +8,109 @@ SCDLLName("Frozen Tundra - Auto Volume by Price")
 
 SCSFExport scsf_AutoVbP(SCStudyInterfaceRef sc)
 {
-    // helper object
-    SCString log_message;
-
-    // which study to reference
-    SCInputRef i_StudyRef = sc.Input[0];
-
-    // magic number multiplier that will determine the granularity of VP bars we'll see as we switch symbols
+	SCInputRef useVap = sc.Input[0];
+    // multiplier that will determine the granularity of VP bars we'll see as we switch symbols
     // the lower the number, the thicker and fewer bars there will be
     // the higher the magic number, the thinner and more VP bars there will be
-    SCInputRef i_TickMultiplier = sc.Input[1];
-
-    SCInputRef useVap = sc.Input[2];
-
+    SCInputRef i_DetailLevel = sc.Input[1];
+	SCInputRef i_Step = sc.Input[2];
+	
+	//number of inputs to set target VBP studies
+	const int MAX_VBP_STUDIES = 20;
+	
     // Configuration
     if (sc.SetDefaults)
     {
         sc.GraphRegion = 0;
-        i_StudyRef.Name = "Select Target Study";
-        i_StudyRef.SetStudyID(0);
-        i_TickMultiplier.Name = "Magic Number Multiplier";
-        i_TickMultiplier.SetFloat(1.3);
-        useVap.Name = "Use VAP instead of magic";
+		sc.GraphName = "Auto set VbPs ticks per bar";
+		sc.AutoLoop = 0; 
+		//update always so we don't have to wait for incoming ticks (market closed)
+		sc.UpdateAlways = 1;
+		
+		//INPUTS CONFIG
+		useVap.Name = "Use VAP setting in Chart Settings";
         useVap.SetYesNo(0);
-
+        i_DetailLevel.Name = "VbPs' detail level";
+        i_DetailLevel.SetInt(100);
+		i_Step.Name = "Tick-size step to increase/decrease detail";
+		i_Step.SetInt(4);
+		
+		for(int x = 0; x < MAX_VBP_STUDIES; x++)
+		{
+			sc.Input[10 + x].Name.Format("%i.Target VbP study", x + 1);
+			sc.Input[10 + x].SetStudyID(0);
+		}
+		
         return;
     }
-
-    int studyId = i_StudyRef.GetStudyID();
-
-    // VbP Ticks Per Volume Bar is input 32, ID 31
+	
+	if(sc.IsFullRecalculation)
+	{
+		//set step to the nearest multiplier of sc.VolumeAtPriceMultiplier to avoid problems
+		int step = i_Step.GetInt();
+		i_Step.SetInt(step - step % sc.VolumeAtPriceMultiplier);
+	}
+	
+	// VbP Ticks Per Volume Bar is input 32, ID 31
     int inputIdx = 31;
 
+	//if setting VbP's Ticks-per-bar according to Volume At Price Multiplier in ChartSettings
     if (useVap.GetInt() == 1) {
-        sc.SetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, sc.VolumeAtPriceMultiplier);
+		
+		for(int x = 0; x < MAX_VBP_STUDIES; x++)
+		{
+			int studyId = sc.Input[10 + x].GetStudyID();
+			if(studyId != 0)
+			{
+				int prevValue;
+				sc.GetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, prevValue); 			
+				if(prevValue != sc.VolumeAtPriceMultiplier)
+					sc.SetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, sc.VolumeAtPriceMultiplier);
+			}
+		}
+		
         return;
     }
-
+	
+	//if using detail level specified in inputs
     float vHigh, vLow, vDiff;
-    float vMultiplier = i_TickMultiplier.GetFloat();
+    int detail = i_DetailLevel.GetInt();
 
     // fetch the graph's price scale's high and low value so we can automate the Ticks setting on VbP
     sc.GetMainGraphVisibleHighAndLow(vHigh, vLow);
 
     // calc the range of visible prices
-    vDiff = (vHigh - vLow);
+	//DIVIDE BY TICK-SIZE TO GET NUMBER OF VISIBLE TICKS
+    vDiff = (vHigh - vLow) / sc.TickSize;
 
-    // divide range by magic number to get the desired VbP Ticks Per Bar value
-    int targetTicksPerBar = max(sc.Round(vDiff / vMultiplier), 1);
+    // divide range by detail level to get the desired VbP Ticks Per Bar value and don't allow it to be less than the VAP multiplier
+    int targetTicksPerBar = max(sc.Round(vDiff / detail), sc.VolumeAtPriceMultiplier);
+	
+	// adapt targetTicksPerBar to the specified step
+	if(targetTicksPerBar >= i_Step.GetInt() )
+		targetTicksPerBar -= targetTicksPerBar % i_Step.GetInt();
+	//if targetTicksPerBar is less than the step but greater than VAP multiplier, then modify it as a VAP multiplier multiple
+	else if(targetTicksPerBar > sc.VolumeAtPriceMultiplier)
+		targetTicksPerBar -= targetTicksPerBar % sc.VolumeAtPriceMultiplier;
+	
+	//flag to redraw chart in case we change any target-VbP
+	bool redraw = false;
+	
+	for(int x = 0; x < MAX_VBP_STUDIES; x++)
+	{
+		int studyId = sc.Input[10 + x].GetStudyID();
+		if(studyId != 0)
+		{
+			int prevValue;
+			sc.GetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, prevValue);
 
-    int prevValue;
-    sc.GetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, prevValue);
-
-    // require at least a 20% change before updating the VbP study
-    if (abs(prevValue - targetTicksPerBar) > (targetTicksPerBar * 0.20)) {
-        // don't allow a Ticks Per Bar value of less than 1
-        if (targetTicksPerBar < 1) {
-            targetTicksPerBar = 1;
-        }
-        sc.SetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, targetTicksPerBar);
-        sc.RecalculateChart(sc.ChartNumber);
-    }
+			if(targetTicksPerBar != prevValue) 
+			{
+				sc.SetChartStudyInputInt(sc.ChartNumber, studyId, inputIdx, targetTicksPerBar);
+				redraw = true;
+			}
+		}
+	}
+	
+	if(redraw) sc.RecalculateChart(sc.ChartNumber);
 }
